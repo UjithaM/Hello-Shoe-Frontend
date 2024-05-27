a0195f154f46ba41fd576e98e5a1bd3d832813e2
Ujitha <ujithamigara@gmail.com>
2024-05-27 09:51:42 +0530

    add supplier DTO

---
+++ dto/SupplierDTO.js
@@ -0,0 +1,16 @@
+class SupplierDTO {
+    constructor(supplierCode, name, category, addressNo, lane, mainCity, mainState, postalCode, country, mobileNumber, landlineNumber, email) {
+        this.supplierCode = supplierCode;
+        this.name = name;
+        this.category = category;
+        this.addressNo = addressNo;
+        this.lane = lane;
+        this.mainCity = mainCity;
+        this.mainState = mainState;
+        this.postalCode = postalCode;
+        this.country = country;
+        this.mobileNumber = mobileNumber;
+        this.landlineNumber = landlineNumber;
+        this.email = email;
+    }
+}
