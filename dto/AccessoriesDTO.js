ce018e0287f98fc61df4311a06ff7815228b2d52
Ujitha <ujithamigara@gmail.com>
2024-05-31 11:37:11 +0530

    add AccessoriesDTO.js and implement it

---
+++ dto/AccessoriesDTO.js
@@ -0,0 +1,14 @@
+class AccessoriesDTO {
+    constructor(accessoriesCode, accessoriesDescription, accessoriesPicture, unitPriceSell, unitPriceBuy, expectedProfit, profitMargin, quantity, accessoriesVerities, supplierCode) {
+        this.accessoriesCode = accessoriesCode;
+        this.accessoriesDescription = accessoriesDescription;
+        this.accessoriesPicture = accessoriesPicture;
+        this.unitPriceSell = unitPriceSell;
+        this.unitPriceBuy = unitPriceBuy;
+        this.expectedProfit = expectedProfit;
+        this.profitMargin = profitMargin;
+        this.quantity = quantity;
+        this.accessoriesVerities = accessoriesVerities;
+        this.supplierCode = supplierCode;
+    }
+}
