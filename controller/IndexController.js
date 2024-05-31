f2182d198169d46d52027a693e0ff548e3d01337
Ujitha <ujithamigara@gmail.com>
2024-05-31 21:40:21 +0530

    something

--- controller/IndexController.js
+++ controller/IndexController.js
@@ -1,6 +1,20 @@
 $(document).ready(function () {
     'use strict';
+    
+    $("#sign_section").css("display", "none");
 
+    $("#deleteCustomerButton").hide();
+    $("#updateCustomerButton").hide();
+
+    $("#suppliers").css("display", "none");
+    $("#customers").css("display", "none");
+    $("#sales").css("display", "none");
+    $("#employee").css("display", "none");
+    $("#inventory").css("display", "none");
+
+    //TODO uncomment this
+    // $("header").hide();
+    // $("main").hide();
     $('.needs-validation').on('submit', function (event) {
         if (this.checkValidity() === false) {
             event.preventDefault();
@@ -10,13 +24,8 @@ $(document).ready(function () {
     });
     $("form").submit(function (e)
     {
-        console.log("form submit called");
         e.preventDefault();
     });
-    $("#suppliers").css("display", "none");
-    $("#customers").css("display", "none");
-    $("#sales").css("display", "none");
-    $("#employee").css("display", "none");
     $('.list-group-item').on('click', function(e){
         e.preventDefault();
         $('.list-group-item').removeClass('active');
@@ -27,12 +36,16 @@ $(document).ready(function () {
         $("#dashboard").css("display", "none");
         $("#sales").css("display", "none");
         $("#employee").css("display", "none");
+        $("#inventory").css("display", "none");
        
         var section = $(this).data('section');
         $(this.hash).css("display", "block");
         $('.content-section').removeClass('active');
         $('#' + section).addClass('active');
     });
+    //TODO remove this
+    $("#login_section").css("display", "none");
+    $("#dashboard").css("display", "none");
 });
 
 
